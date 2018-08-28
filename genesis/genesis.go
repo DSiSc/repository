b08e9898027902a8cbbe7d821f4219c678e534c6
walterBishop <walterkangluo@gmail.com>
2018-08-28 17:56:17 +0800

    feature# Adding genesis.json

--- genesis/genesis.go
+++ genesis/genesis.go
@@ -5,9 +5,19 @@ import (
 	"time"
 )
 
+type GenesisBlockConf struct {
+	PrevBlockHash types.Hash
+	TxRoot        types.Hash
+	StateRoot     types.Hash
+	ReceiptsRoot  types.Hash
+	Height        uint64
+	Timestamp     uint64
+	MixDigest     types.Hash
+}
+
 type GensisBlock struct {
-	block     *types.Block     // genesis to a block
-	extraData []byte           // extra data of genesis
+	block     *types.Block
+	extraData []byte
 }
 
 func BuildGensisBlock() (*GensisBlock, error) {
@@ -15,9 +25,9 @@ func BuildGensisBlock() (*GensisBlock, error) {
 		PrevBlockHash: types.Hash{},
 		TxRoot:        types.Hash{},
 		StateRoot:     types.Hash{},
-		ReceipsRoot:   types.Hash{},
+		ReceiptsRoot:  types.Hash{},
 		Height:        uint64(0),
-		Timestamp:     uint64(time.Date(2018, time.June, 30, 0, 0, 0, 0, time.UTC).Unix()),
+		Timestamp:     uint64(time.Date(2018, time.August, 28, 0, 0, 0, 0, time.UTC).Unix()),
 		MixDigest:     types.Hash{},
 		SigData:       nil,
 	}
