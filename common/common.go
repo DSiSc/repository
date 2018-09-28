6ccc9600660477f0dce5f0d4ee83142bab240f14
bchain618 <bchain618@gmail.com>
2018-09-28 12:18:47 +0800

    bugfix# refactor TxHash func
    1.update the way to calculate tx's hash

--- common/common.go
+++ common/common.go
@@ -22,14 +22,26 @@ func Sum(bz []byte) []byte {
 
 // TxHash calculate tx's hash
 func TxHash(tx *types.Transaction) (hash types.Hash) {
-	if nil == tx {
-		log.Warn("Tx is nil, so it's hash meaningless.")
-		return
+	if hash := tx.Hash.Load(); hash != nil {
+		return hash.(types.Hash)
 	}
-	jsonByte, _ := json.Marshal(tx)
+	hashData := types.TxData{
+		AccountNonce: tx.Data.AccountNonce,
+		Price:        tx.Data.Price,
+		GasLimit:     tx.Data.GasLimit,
+		Recipient:    tx.Data.Recipient,
+		Amount:       tx.Data.Amount,
+		Payload:      tx.Data.Payload,
+		V:            tx.Data.V,
+		R:            tx.Data.R,
+		S:            tx.Data.S,
+	}
+	jsonByte, _ := json.Marshal(hashData)
 	sumByte := Sum(jsonByte)
-	copy(hash[:], sumByte)
-	return
+	var temp types.Hash
+	copy(temp[:], sumByte)
+	tx.Hash.Store(temp)
+	return temp
 }
 
 func HeaderHash(block *types.Block) (hash types.Hash) {
